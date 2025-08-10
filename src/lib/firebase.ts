18bc4d7fa11d63a88b0020f6bd55416260ac2e1e
Berkan SIĞIRCI <berkansigirci1225@gmail.com>
2025-08-10 20:13:41 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "apiKey": "YOUR_API_KEY",
-  "authDomain": "YOUR_AUTH_DOMAIN",
-  "projectId": "YOUR_PROJECT_ID",
-  "storageBucket": "YOUR_STORAGE_BUCKET",
-  "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
-  "appId": "YOUR_APP_ID"
+  "projectId": "berkan-matematik-pkotx",
+  "appId": "1:641384261184:web:7e9ae15584ea7f829421a5",
+  "storageBucket": "berkan-matematik-pkotx.firebasestorage.app",
+  "apiKey": "AIzaSyB673gxS9PuXNJH-5vUP98zcunCAZ9ja80",
+  "authDomain": "berkan-matematik-pkotx.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "641384261184"
 };
 
 
